d72aa817e65a207579522a77233db6b43f9fc9a2
Larry Prall <lprall@mac.com>
2022-03-06 16:02:24 -0500

    First working version (from play)

--- lib/json_api.ex
+++ lib/json_api.ex
@@ -1,7 +1,7 @@
 defmodule JsonAPI do
 
   def query(cat,id,keys) do
-    
+
     categories = %{
       "posts"    => 100,
       "comments" => 500,
@@ -12,108 +12,102 @@ defmodule JsonAPI do
     }
 
     if cat not in Map.keys(categories) do
-      {:error, ~s('#{cat}' is not a valid category.) }
-    else
-      if id > categories[cat] do
-        {:error, ~s(The maximum id for '#{cat}' is #{categories[cat]}.) }
-      else 
-        base = "https://jsonplaceholder.typicode.com/"
-        [base, cat, "/", to_string(id)]
-              |> :erlang.iolist_to_binary
-              |> HTTPoison.get
-              |> handle_response(keys)
-      end
-    end
-  end
+       {:error, ~s('#{cat}' is not a valid category.) }
+     else
+       if id > categories[cat] do
+         {:error, ~s(The maximum id for '#{cat}' is #{categories[cat]}.) }
+       else
+         base = "https://jsonplaceholder.typicode.com/"
+         [base, cat, "/", to_string(id)]
+               |> :erlang.iolist_to_binary
+               |> HTTPoison.get
+               |> handle_response(keys)
+       end
+     end
+   end
 
-  def query(cat,id) do
-    
-    categories = %{
-      "posts"    => 100,
-      "comments" => 500,
-      "albums"   => 100,
-      "photos"   => 5000,
-      "todo"     => 200,
-      "users"    => 10
-    }
+   def query(cat,id) do
 
-    if cat not in Map.keys(categories) do
-      {:error, ~s('#{cat}' is not a valid category.) }
-    else
-      if id > categories[cat] do
-        {:error, ~s(The maximum id for '#{cat}' is #{categories[cat]}.) }
-      else 
-        base = "https://jsonplaceholder.typicode.com/"
-        [base, cat, "/", to_string(id)]
-              |> :erlang.iolist_to_binary
-              |> HTTPoison.get
-              |> handle_response
-      end
-    end
-  end
+     categories = %{
+       "posts"    => 100,
+       "comments" => 500,
+       "albums"   => 100,
+       "photos"   => 5000,
+       "todos"     => 200,
+       "users"    => 10
+     }
 
-  def query(cat) do
-    
-    categories = %{
-      "posts"    => 100,
-      "comments" => 500,
-      "albums"   => 100,
-      "photos"   => 5000,
-      "todo"     => 200,
-      "users"    => 10
-    }
+     if cat not in Map.keys(categories) do
+       {:error, ~s('#{cat}' is not a valid category.) }
+     else
+       if id > categories[cat] do
+         {:error, ~s(The maximum id for '#{cat}' is #{categories[cat]}.) }
+       else
+         base = "https://jsonplaceholder.typicode.com/"
+         [base, cat, "/", to_string(id)]
+               |> :erlang.iolist_to_binary
+               |> HTTPoison.get
+               |> handle_response
+       end
+     end
+   end
 
-    if cat not in Map.keys(categories) do
-      {:error, ~s('#{cat}' is not a valid category.) }
-    else
-        base = "https://jsonplaceholder.typicode.com/"
-        [base, cat]
-              |> :erlang.iolist_to_binary
-              |> HTTPoison.get
-              |> handle_response
-    end
-  end
+   def query(cat) do
 
-  def handle_response( {:ok, %{status_code: 200, body: body} = _response}, keys ) do
-    target = body
-           |> Poison.Parser.parse!(%{})
-           |> get_in(keys)
+     categories = %{
+       "posts"    => 100,
+       "comments" => 500,
+       "albums"   => 100,
+       "photos"   => 5000,
+       "todos"     => 200,
+       "users"    => 10
+     }
 
-    {:ok, target}
-  end
+     if cat not in Map.keys(categories) do
+       {:error, ~s('#{cat}' is not a valid category.) }
+     else
+         base = "https://jsonplaceholder.typicode.com/"
+         [base, cat]
+               |> :erlang.iolist_to_binary
+               |> HTTPoison.get
+               |> handle_response
+     end
+   end
 
-  def handle_response( {:ok, %{status_code: status, body: body} = _response}, _keys) do
-    message = body 
-              |> Poison.Parser.parse!(%{})
-              |> get_in(["message"])
-    {:error, status, message }
-  end
+   def handle_response( {:ok, %{status_code: 200, body: body} = _response}, keys ) do
+     target = body
+            |> Poison.Parser.parse!(%{})
+            |> get_in(keys)
 
-  def handle_response( {:error, reason }, _ ) do
-    {:error, reason}
-  end
+     {:ok, target}
+   end
 
-  def handle_response( {:ok, %{status_code: 200, body: body} = _response}) do
-    target = body
-           |> Poison.Parser.parse!(%{})
+   def handle_response( {:ok, %{status_code: status, body: body} = _response}, _keys) do
+     message = body
+               |> Poison.Parser.parse!(%{})
+               |> get_in(["message"])
+     {:error, status, message }
+   end
 
-    {:ok, target}
-  end
+   def handle_response( {:error, reason }, _ ) do
+     {:error, reason}
+   end
 
-  def handle_response( {:ok, %{status_code: status, body: body} = _response}) do
-    message = body 
-              |> Poison.Parser.parse!(%{})
-              |> get_in(["message"])
-    {:error, status, message }
-  end
+   def handle_response( {:ok, %{status_code: 200, body: body} = _response}) do
+     target = body
+            |> Poison.Parser.parse!(%{})
 
-  def handle_response( {:error, reason }) do
-    {:error, reason}
-  end
-end
+     {:ok, target}
+   end
 
+   def handle_response( {:ok, %{status_code: status, body: body} = _response}) do
+     message = body
+               |> Poison.Parser.parse!(%{})
+               |> get_in(["message"])
+     {:error, status, message }
+   end
 
-
-  
-    
-
+   def handle_response( {:error, reason }) do
+     {:error, reason}
+   end
+ end
