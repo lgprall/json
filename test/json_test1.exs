992070b4e0e84a418c31d18a6e6c6a8f07a2c6a5
lgprall <30762998+lgprall@users.noreply.github.com>
2022-03-10 15:06:56 -0500

    Adding the new pattern-matching version from droll (Mark Brown)

---
+++ test/json_test1.exs
@@ -0,0 +1,47 @@
+defmodule JsonTest do
+  use ExUnit.Case
+#  doctest JsonTest
+
+  test "rejects invalid categories" do
+    response = JsonAPI.query("zork")
+
+    assert response == "'zork' is invalid"
+  end
+  
+  test "rejects out of bounds id" do
+    response = JsonAPI.query("posts",101)
+
+    assert response == "'posts' max id: 100"
+  end
+
+  test "reports correct data for cat/id" do
+    response = JsonAPI.query("posts",1)
+
+    assert response == {:ok,
+	   %{
+		 "body" => "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+		 "id" => 1,
+		 "title" => "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", 
+		 "userId" => 1
+	   }}
+  end
+
+  test "reports correct data for single key" do
+    response = JsonAPI.query("users",10,["address"])
+
+    assert response ==  {:ok,
+ %{
+   "city" => "Lebsackbury",
+   "geo" => %{"lat" => "-38.2386", "lng" => "57.2232"},
+   "street" => "Kattie Turnpike",
+   "suite" => "Suite 198",
+   "zipcode" => "31428-2261"
+ }}
+  end
+  test "reports correct data for multiple key" do
+	response = JsonAPI.query("users",10,["address","geo"])
+
+	assert response == {:ok, %{"lat" => "-38.2386", "lng" => "57.2232"}}
+  end
+
+end
